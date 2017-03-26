9ca3e76e3554a36647d7ad028c7a5ee8ab1e8fd8
Caroline Olijve <olijvecaroline@gmail.com>
2017-03-27 00:02:02 +0200

    fixed hangmangame

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,14 +1,13 @@
 require_relative "random_word.rb"
 
 class Hangman
-attr_reader :random_word, :answer, :array_word, :bad_guesses_left, :answers
-attr_accessor :letter_counter, :playground
+attr_reader :array_word, :random_word, :answers
 
-    def initialize (letter_counter=0)
+    def initialize
       @random_word = RandomWord.new.word.downcase
       @array_word = @random_word.split(//)
       @bad_guesses_left = 10
-      @letter_counter = letter_counter
+      @letter_counter = 0
       @answers=[]
       @playground =([" _ "]*(@array_word.length))
     end
@@ -18,71 +17,67 @@ attr_accessor :letter_counter, :playground
       puts " the unknown word has #{@array_word.length} letters"
     end
 
-    def print_playground
-      @playground.each {|x| print " #{x.upcase} "}
-    end
-
-    def self.turn
+    def turn
       print "Give us a letter, or, to guess the word, type '!': "
       @answer = gets.chomp.chr.downcase
     end
 
-      def input_check
-        case @answer
-        when "!"
-          print " ! : "
-          @guess = gets.chomp.downcase
-            if
-              @guess == @random_word
-              ending
-            else
-              print "Wrong, you get -1 chances \n\n"
-              @bad_guesses_left -= 1
-              turn
-            end
-        when ""
-            print "Don's hit enter, type a letter. "
+    def input_check
+      case @answer
+      when "!"
+        print " ! : "
+        @guess = gets.chomp.downcase
+          if
+            @guess == @random_word
+            win
+          else
+            puts "Wrong, you get -1 chances \n\n"
+            @bad_guesses_left -= 1
             turn
-          #when (@answers.include? @answer) = true
-          #puts @answers
-          #print " You already tried that letter before , choose another. "
-          #turn
-        when
-            !(%w(a b c d e f g h i j k l m n o p q r s t u  v w x  y z).include? @answer)
-            print " Please provide only letters, to quess the word type '!'. "
-        else
-          add_answer_to_answers
-        end
+          end
+      when ""
+          puts "Don's hit enter, type a letter. "
+          turn
       end
+    end
 
-  def add_answer_to_answers
-    @answers<<@answer
-  end
-
-  def letter_check
-      if (@array_word.include? @answer)
-        @array_word.each do |x|
-          x.downcase
-                x == @answer
-                y = @array_word.index(x)
-                @playground[y] = @answer
-                @letter_counter += 1
-            end
-      else
-        @bad_guesses_left -= 1
-        puts "too bad"
+    def input_checkII
+      if !(%w(a b c d e f g h i j k l m n o p q r s t u  v w x  y z).include? @answer)
+        puts " Please provide only letters, to quess the word type '!'. "
+        turn
+      elsif (@answers.include? @answer)
+        puts" You already tried that letter before , choose another. "
+        turn
       end
-  end
+      @answer
+    end
 
-    def duration
-      while (@bad_guesses_left > 0) && (@letter_counter != @array_word.length)  do
-        Hangman.turn
-        input_check
-        letter_check
-        print_playground
-        puts "bad_guesses_left: #{@bad_guesses_left}"
-        puts  "#{(array_word.length)-@letter_counter} letters to go"
-      end
+
+
+
+    def add_answer_to_answers
+        @answers<<@answer
+    end
+
+    def letter_check
+          if
+            (@array_word.include? @answer)
+            @array_word.each do |x|
+              x.downcase
+                    if
+                      x == @answer
+                      y = @array_word.index(x)
+                      @playground[y] = @answer
+                      @letter_counter += 1
+                    end
+                end
+
+          else
+            @bad_guesses_left -= 1
+            puts "too bad"
+          end
+          @playground.each {|x| print " #{x.upcase} "}
+          puts "these letters have been tried: #{@answers}"
     end
 
     def hangman
@@ -106,23 +101,38 @@ attr_accessor :letter_counter, :playground
       end
     end
 
-    def win
+    def win_without_guess
       if @letter_counter == @array_word.length
-        puts "Hing hang hong the hangman 's won'"
+          win
       end
     end
 
-    def ending
-      win
-      dead
+    def win
+        puts "Hing hang hong the hangman 's won'"
     end
 
     def play!
       puts "Hi from lib\hangman.rb!"
+
       start
-      duration
-      ending
+
+      while (@bad_guesses_left > 0) && (@letter_counter != @array_word.length)  do
+
+      turn
+      input_check
+      input_checkII
+      add_answer_to_answers
+      letter_check
+
+      puts "bad_guesses_left: #{@bad_guesses_left}"
+      puts "#{(@array_word.length)-@letter_counter} letters to go"
+      end
+
+      win_without_guess
+      dead
     end
+
+
 end
 
 
